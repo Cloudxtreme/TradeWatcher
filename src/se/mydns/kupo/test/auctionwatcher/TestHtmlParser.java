3b2e986038fc0420507732424c36507b180d23c4
oskurot <oskurot@gmail.com>
2014-06-22 12:41:36 +0200

    Added testcase.

---
+++ src/se/mydns/kupo/test/auctionwatcher/TestHtmlParser.java
@@ -0,0 +1,14 @@
+package se.mydns.kupo.test.auctionwatcher;
+
+import org.junit.Test;
+
+/**
+ * Created by oskurot on 22/06/14.
+ */
+public class TestHtmlParser {
+
+    @Test
+    public void testHtmlParser() {
+
+    }
+}
