d8b51060a11de920921915865bfb86d7c312beb7
oskurot <oskurot@gmail.com>
2014-06-24 14:36:22 +0200

    Started working on the layout.

--- src/se/mydns/kupo/auctionwatcher/WatchListFrame.java
+++ src/se/mydns/kupo/auctionwatcher/WatchListFrame.java
@@ -17,15 +17,27 @@ public class WatchListFrame implements Runnable {
 
 
     public void run() {
+        /** New shiny window **/
         JFrame frame = new JFrame("Project 1999 Auction Watcher");
-
-        frame.setPreferredSize(new Dimension(200,100));
+        frame.setPreferredSize(new Dimension(200, 100));
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage(".\\res\\eq.png"));
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /** Layout **/
+        GridLayout gridLayout = new GridLayout(0,2);
+        frame.setLayout(gridLayout);
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBackground(Color.blue);
+        JPanel rightPanel = new JPanel();
+        rightPanel.setBackground(Color.red);
+
+        frame.add(leftPanel);
+        frame.add(rightPanel);
         frame.pack();
         frame.setVisible(true);
 
+        /** System tray **/
         TrayIcon trayIcon = null;
         if(SystemTray.isSupported()) {
             SystemTray tray = SystemTray.getSystemTray();
@@ -50,6 +62,9 @@ public class WatchListFrame implements Runnable {
             } catch (AWTException e) {
                 e.printStackTrace();
             }
+
+
+//            trayIcon.displayMessage("Caption", "This is the message", TrayIcon.MessageType.INFO);
         } else {
             System.err.println("Tray unavailable");
         }
